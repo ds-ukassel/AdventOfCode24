0a67f3a62b1d995cf357c712103ee40778e31c66
Frank Hessel <fhessel@seemoo.tu-darmstadt.de>
2022-12-01 11:40:07 +0100

    Fix workflow script

--- .github/scripts/update-workflow.sh
+++ .github/scripts/update-workflow.sh
@@ -3,7 +3,7 @@ SCRIPTDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 OUTPUT="$SCRIPTDIR/../workflows/benchmark.yml"
 
 cat "$SCRIPTDIR/benchmark0.template" > "$OUTPUT"
-for BRANCH in "$(cat "$SCRIPTDIR/branches")"; do
+while read -r BRANCH; do
 cat <<EOF >> "$OUTPUT"
       - name: Checkout $BRANCH
         uses: actions/checkout@v2
@@ -11,6 +11,6 @@ cat <<EOF >> "$OUTPUT"
           path: "./$BRANCH"
           ref: "$BRANCH"
 EOF
-done
+done < "$SCRIPTDIR/branches"
 
 cat "$SCRIPTDIR/benchmark1.template" >> "$OUTPUT"
